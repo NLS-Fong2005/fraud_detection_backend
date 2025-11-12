d60b1dad328443cd333bdb546bb053e1042b4ce0
Nixon@Macrokiosk <nixon.fong@macrokiosk.com>
2025-11-12 09:50:47 +0800

    feat: Implemented Network Data Examiner Agent and Temporal Data Examiner Agent

--- src/machine_learning/llm_rag_method/llm_rag_spam_classifier.py
+++ src/machine_learning/llm_rag_method/llm_rag_spam_classifier.py
@@ -1,4 +1,6 @@
 # <--- Imports --->
+from langchain.agents import create_agent
+from langchain.tools import tool
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_openai import ChatOpenAI
 from dotenv import load_dotenv
@@ -18,12 +20,13 @@ class LlmRagSpamClassifier:
     def __read_message_content__(self, message_content: str):
         message_agent_prompt = ChatPromptTemplate.from_template(
             """
-            TASK: Your sole task is to decide whether the given input is SPAM or HAM based on the Guidelines Provided.
+            # TASK 
+            Your sole task is to decide whether the given input is SPAM or HAM based on the Guidelines Provided.
 
-            RULES: 
-                1. Reply in this format: "DECISION: Explanation"
-                    - DECISION = TRUE if SPAM
-                    - DECISIONS = FALSE if HAM
+            # RULES: 
+                1. Reply in this format: "TRUE/FALSE: Explanation"
+                    - TRUE if SPAM
+                    - FALSE if HAM
                     - Divulge into your explanation based on the guidelines.
 
             Guidelines: {guidelines}
@@ -34,11 +37,9 @@ class LlmRagSpamClassifier:
 
         message_agent_chain = message_agent_prompt | self.LLM
 
-        guidelines = vector_collection.fetch_object_from_header(message_guideline)
-
         agent_reply = message_agent_chain.invoke(
             {
-                "guidelines": guidelines,
+                "guidelines": vector_collection.fetch_object_from_header(message_guideline),
                 "input": message_content
             }
         )
@@ -46,25 +47,80 @@ class LlmRagSpamClassifier:
         return agent_reply.content
 
 
-    def __examine_network_data__(self):
+    def __examine_network_data__(self, network_data: str):
+        network_guideline: str = "network_guideline"
+        
         network_data_examiner_prompt = ChatPromptTemplate.from_template(
             """
+            # TASK
+            Your sole task is to decide whether the given input is SPAM or HAM basde on the guidelines provided.
+
+            # RULES:
+                1. Reply in this format: "TRUE/FALSE: Explanation"
+                    - TRUE if given network is within SUSPICIOUS IP ADDRESS RANGES (SUSPICIOUS/SPAM LIKELY)
+                    - FALSE if given network is SAFE/within SAFE IP ADDRESS RANGES (HAM)
+                    - Divulge into your explanation based on the guidelines.
+            
             Guidelines: {guidelines}
             Human: {input}
             """
         )
+
+        network_agent_chain = network_data_examiner_prompt | self.LLM
+
+        agent_reply = network_agent_chain.invoke(
+            {
+                "guidelines": vector_collection.fetch_object_from_header(network_guideline),
+                "input": network_data
+            }
+        )
+
+        return agent_reply.content
+    
+    def __examine_temporal_data__(self, hour_sent: int):
+        @tool("is_suspicious_hour", description="Takes the hour input and returns a boolean. If TRUE, the hour at which message was sent is suspicious. If False, the hour at which message was sent is not likely to be spam.")
+        def is_suspicious_hour(hour: str) -> bool:
+            hour_int: int = int(hour)
+            if (hour_int < 6):
+                return True
+            else:
+                return False
+        
+        temporal_data_examiner_prompt: str = """
+            # TASK
+            Your sole task is to decide whether the given temporal input is SPAM or HAM based on the output of the tool provided.
+
+            # RULES
+                1. Always use the the tool "is_suspicious_hour" to base your answer upon.
+                2. Reply in this format: "TRUE/FALSE: Explanation"
+                    - TRUE if hour is SPAM based on Tool Output
+                    - FALSE if hour is HAM based on Tool Output
+            """
+
+        agent = create_agent(
+            self.LLM,
+            tools=[is_suspicious_hour],
+            system_prompt=str(temporal_data_examiner_prompt)
+        )
+
+        agent_reply = agent.invoke(
+            {"messages": [{"role": "user", "content": str(hour_sent)}]}
+        )
+
+        return agent_reply["messages"][-1].content
 
     def __examine_geographical_data__(self, geographical_data: str):
         geography_guideline: str = "geography_guideline"
 
         geography_examiner_prompt = ChatPromptTemplate.from_template(
             """
-            TASK: Your sole task is to decide whether the given input is SPAM or HAM based on the Guidelines Provided.
+            # TASK
+            Your sole task is to decide whether the given input is SPAM or HAM based on the Guidelines Provided.
 
-            RULES: 
-                1. Reply in this format: "DECISION: Explanation"
-                    - DECISION = TRUE if Geography is SUSPICIOUS/SPAM LIKELY
-                    - DECISIONS = FALSE if Geography is SAFE/HAM
+            # RULES: 
+                1. Reply in this format: "TRUE/FALSE: Explanation"
+                    - TRUE if Geography is SUSPICIOUS/SPAM LIKELY
+                    - FALSE if Geography is SAFE/HAM
                     - Divulge into your explanation based on the guidelines.
 
             Guidelines: {guidelines}
@@ -74,21 +130,21 @@ class LlmRagSpamClassifier:
 
         geography_agent_chain = geography_examiner_prompt | self.LLM
 
-        guidelines = vector_collection.fetch_object_from_header(geography_guideline)
-
         agent_reply = geography_agent_chain.invoke(
             {
-                "guidelines": guidelines,
+                "guidelines": vector_collection.fetch_object_from_header(geography_guideline),
                 "input": geographical_data
             }
         )
 
         return agent_reply.content
 
-    def __classifier_agent__(self):
+    def classifier_agent(self):
         classifier_agent_prompt = ChatPromptTemplate.from_template(
             """
-            Guidelines: {guidelines}
+            # TASK
+            Your sole task is to determine how likely a message is suspicious of being spam based on the number of true statements for four columns. 
+            Use the provided tool to help calculate.
             Human: {input}
             """
         )
@@ -96,34 +152,4 @@ class LlmRagSpamClassifier:
 llm_rag_spam_classifier = LlmRagSpamClassifier()
 
 if __name__ == "__main__":
-    print("\n--- Message Content ---\n")
-    messages = [
-        # Spam
-        "Congratulations! You've won a free iPhone. Click the link to claim now!",
-        "URGENT: Your bank account has been suspended. Verify your details immediately.",
-        "You've been selected for a £5000 reward. Reply YES to receive it today!",
-
-        # Ham
-        "Hey, are we still meeting for lunch later?",
-        "Don't forget the team call at 3pm, yeah?",
-        "Could you send me the report when you're done?"
-    ]
-
-    for message in messages:
-        print(llm_rag_spam_classifier.__read_message_content__(message))
-
-    print("\n--- Geography Content ---\n")
-    sample_geography = [
-        ("Malaysia", "Kuala Lumpur"),
-        ("Singapore", "Central"),
-        ("Indonesia", "Bali"),
-        ("Thailand", "Chiang Mai"),
-        ("United States", "Virginia"),
-        ("China", "Beijing"),
-        ("Netherlands", "North Holland"),
-        ("Russia", "Moscow"),
-        ("United Kingdom", "Greater London"),
-    ]
-
-    for geo in sample_geography:
-        print(llm_rag_spam_classifier.__examine_geographical_data__(str(geo)))
+    print(llm_rag_spam_classifier.__examine_temporal_data__(hour_sent=12))
