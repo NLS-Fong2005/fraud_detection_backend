a7499c27d497f6726eff046ffb33dbe66bab6fcb
Nixon@Macrokiosk <nixon.fong@macrokiosk.com>
2025-11-21 15:39:40 +0800

    feat: Implemented error handling of missing required columns

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 # <--- Imports --->
-import io
 import pandas as pd
 import json
 from fastapi import (
@@ -9,22 +8,63 @@ from fastapi import (
 )
 from fastapi.responses import StreamingResponse
 
+from src.machine_learning.llm_rag_method.models.object_model import DataObject
+
 app = FastAPI()
 
 @app.get("/health_check")
 async def health_check():
     return {"status": "Healthy"}
 
+def is_column_in(dataframe: pd.DataFrame) -> bool:
+    if "Message" not in dataframe.columns:
+        return False
+    
+    if "Sent_Time" not in dataframe.columns:
+        return False
+    
+    if "Source_IP" not in dataframe.columns:
+        return False
+
+    if "Source_Location" not in dataframe.columns:
+        return False
+    
+    return True
+
 def process_csv(file_object):
     try:
         csv_chunks = pd.read_csv(file_object, chunksize=10)
 
+        first_chunk = True
+
+
         for chunk in csv_chunks:
+            if first_chunk:
+                if not is_column_in(dataframe=chunk):
+                    error_detail = json.dumps(
+                        {
+                            "error": f"Invalid CSV Format: Missing required columns."
+                        }
+                    )
+
+                    yield f"{error_detail}\n"
+                    return
+
+                first_chunk = False
+
             chunk.dropna(inplace=True)
 
             for _, row in chunk.iterrows():
                 row_dict = row.to_dict()
-                row_json = json.dumps(row_dict)
+                
+                row_object: DataObject = DataObject(
+                    message_content=row_dict["Message"],
+                    sent_time=row_dict["Sent_Time"],
+                    source_ip=row_dict["Source_IP"],
+                    source_location=row_dict["Source_Location"]
+                )
+
+                row_json = row_object.model_dump_json()
 
                 yield f"{row_json}\n"
     except Exception as e:
